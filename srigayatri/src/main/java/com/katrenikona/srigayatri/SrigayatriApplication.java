a70525df5e0db98ac5f83b47ee3b0eda8583c346
Kartik Modukuri <kartikmodukuri@Kartiks-MacBook-Air.local>
2022-04-05 12:08:16 +0530

    Postgres added

--- srigayatri/src/main/java/com/katrenikona/srigayatri/SrigayatriApplication.java
+++ srigayatri/src/main/java/com/katrenikona/srigayatri/SrigayatriApplication.java
@@ -2,9 +2,10 @@ package com.katrenikona.srigayatri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SrigayatriApplication {
+public class SrigayatriApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SrigayatriApplication.class, args);
